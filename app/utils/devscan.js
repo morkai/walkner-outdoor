17a8af015759f4d530b78a67442f3d49d8bcb52a
Łukasz Walukiewicz <lukasz@walukiewicz.eu>
2012-11-13 16:28:27 +0100

    Made the devscan diag utility more universal.

--- app/utils/devscan.js
+++ app/utils/devscan.js
@@ -6,7 +6,7 @@ var diagConfig = require('../../config/diag');
 var libcoapConfig = require('../../config/libcoap');
 
 var devscanRegExp = new RegExp('\\[([0-9a-f:]+)\\]via\\[([0-9a-f:]+)\\]', 'g');
-var macFromIpRegExp = /^[0-9a-f]{4}::2([0-9a-f]{2}):([0-9a-f]{2})ff:fe([0-9a-f]{2}):([0-9a-f]{2})([0-9a-f]{2})/i;
+var macFromIpRegExp = /^[0-9a-f]{4}:[0-9a-f]{4}:[0-9a-f]{4}:[0-9a-f]{4}:([0-9a-f]{2})([0-9a-f]{2}):([0-9a-f]{2})[0-9a-f]{2}:[0-9a-f]{2}([0-9a-f]{2}):([0-9a-f]{2})([0-9a-f]{2})$/;
 var lastDevscanPayload = null;
 
 exports.lastResult = {
@@ -15,11 +15,12 @@ exports.lastResult = {
   nodes: []
 };
 
+var coordinatorIp = expandIpv6(diagConfig.coordinatorIp);
+var coordinatorMac = extractMacFromIpv6(coordinatorIp);
+
 exports.scan = function(done)
 {
-  var coordinatorIp = diagConfig.coordinatorIp;
-
-  if (!diagConfig.coordinatorIp)
+  if (!coordinatorIp)
   {
     return done("Device scanning is disabled.");
   }
@@ -93,14 +94,15 @@ function parseLastDevscanPayload(macToIdMap)
 
   while ((match = devscanRegExp.exec(lastDevscanPayload)) !== null)
   {
-    var sourceIp = match[1];
-    var targetIp = match[2];
-    var sourceMac = extractMacFromIp(sourceIp);
-    var targetMac = extractMacFromIp(targetIp);
+    var sourceIp = expandIpv6(match[1]);
+    var targetIp = expandIpv6(match[2]);
+    var sourceMac = extractMacFromIpv6(sourceIp);
+    var targetMac = extractMacFromIpv6(targetIp);
 
     if (targetMac === sourceMac)
     {
-      targetMac = extractMacFromIp(diagConfig.coordinatorIp);
+      targetIp = coordinatorIp;
+      targetMac = coordinatorMac;
     }
 
     var sourceId = macToIdMap[sourceMac];
@@ -128,21 +130,109 @@ function parseLastDevscanPayload(macToIdMap)
   exports.lastResult.nodes = nodes;
 }
 
-function extractMacFromIp(ip)
+function extractMacFromUri(uri)
 {
+  return extractMacFromIpv6(parseUrl(uri).hostname);
+}
+
+/**
+ * @param {?String} address
+ * @return {?String}
+ * @see http://forrst.com/posts/JS_Expand_Abbreviated_IPv6_Addresses-1OR
+ */
+function expandIpv6(address)
+{
+  if (typeof address !== 'string')
+  {
+    return null;
+  }
+
+  if (address.length === 39)
+  {
+    return address;
+  }
+
+  var validGroupCount = 8;
+  var validGroupSize = 4;
+  var fullAddress;
+  var i;
+  var l;
+
+  if (address.indexOf('::') === -1)
+  {
+    fullAddress = address;
+  }
+  else
+  {
+    var sides = address.split('::');
+    var groupsPresent = 0;
+
+    for (i = 0, l = sides.length; i < l; ++i)
+    {
+      groupsPresent += sides[i].split(':').length;
+    }
+
+    fullAddress = sides[0] + ':';
+
+    for (i = 0, l = validGroupCount - groupsPresent; i < l; ++i)
+    {
+      fullAddress += '0000:';
+    }
+
+    fullAddress += sides[1];
+  }
+
+  var groups = fullAddress.split(':');
+
+  if (groups.length !== validGroupCount)
+  {
+    return null;
+  }
+
+  var expandedAddress = '';
+  var lastGroupIndex = validGroupCount - 1;
+
+  for (i = 0; i < validGroupCount; ++i)
+  {
+    while (groups[i].length < validGroupSize)
+    {
+      groups[i] = '0' + groups[i];
+    }
+
+    expandedAddress += groups[i];
+
+    if (i !== lastGroupIndex)
+    {
+      expandedAddress += ':';
+    }
+  }
+
+  return expandedAddress.toLowerCase();
+}
+
+/**
+ * @param {?String} ip
+ * @return {?String}
+ */
+function extractMacFromIpv6(ip)
+{
+  ip = expandIpv6(ip);
+
+  if (ip === null)
+  {
+    return null;
+  }
+
   var parts = ip.match(macFromIpRegExp);
 
   parts.shift();
 
-  return '00:' + parts.join(':');
-}
+  parts[0] = (parseInt(parts[0], 16) ^ 2).toString(16);
 
-function extractMacFromUri(uri)
-{
-  return extractMacFromIp(parseUrl(uri).hostname);
-}
+  if (parts[0].length === 1)
+  {
+    parts[0] = '0' + parts[0];
+  }
 
-function broadcastLinkDifference(oldLinks)
-{
-
+  return parts.join(':');
 }
